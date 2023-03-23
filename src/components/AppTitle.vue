b8783c991320dd551ff824002c40f6eb3958e8ec
alessandroamormino <amormino.alessandro97@gmail.com>
2023-03-23 14:35:10 +0100

    feat: create AppTitle components and import it

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'My first Vite page!',
+        }
+    }
+}
+</script>
+<template>
+    <div class="container-centered">
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+<style>
+.container-centered {
+    display: flex;
+    justify-content: center;
+
+    padding-top: 2em;
+}
+</style>
